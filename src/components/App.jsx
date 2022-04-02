529c9337f529c63e3f6f87a1216647e6ea06a97d
Korobass87 <korobass1987@yandex.ru>
2022-04-02 18:25:28 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics'
 import Section from './Section/Section'
 import Notification from './Notification/Notification'
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
 
- totalFeeds = () => {
-  const {good, neutral, bad} = this.state
-  return good + neutral + bad
+const feed = ["good", "neutral", "bad" ]
 
- } 
 
- positivePercentage = () => {
-  const {good} = this.state
-  const total = this.totalFeeds()
-  return Math.trunc(good / total * 100) 
+export default function App () {
 
- }
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
+  function addFeed (e) {
+    let feedName = e.target.name
+    
+    // рабочий вариант, но не знаю можно ли так делать
+    // eval(`set${feedName[0].toUpperCase() + feedName.slice(1)}`)(eval(feedName) + 1)
+    
+    switch (feedName) {
+      case "good":
+        setGood(good+1);
+        break;
+      case "bad":
+      setBad(bad+1);
+      break;
+      case "neutral":
+        setNeutral(neutral+1);
+      break;    
+      default:
+        break;
+    }}
   
- 
-addFeed = (props) => {
-    const {name} = props.currentTarget        
-    this.setState(prevState => ({
-      [name]:prevState[name] + 1
-    }));
-
-  };
-
-
-  render() 
-    {
-      const {good, neutral, bad} = this.state
-      const total = this.totalFeeds()
-      const positivePercentage = this.positivePercentage()
-     
-    return (
-<>
-<Section title="Please leave feedback">
-  <FeedbackOptions options={Object.keys(this.state)} addFeed={this.addFeed} />  
-</Section>
-<Section title="Statistics">
-  {total ?
-  <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage ? positivePercentage : 0} /> 
-  : <Notification message="There is no feedback"></Notification>}
-      
-</Section>
+  
+  return (
+    <>
+    <Section title="Please leave feedback">
+      <FeedbackOptions options={feed} addFeed={addFeed} />  
+    </Section>
+    <Section title="Statistics">
+      {(good > 0 || neutral > 0 || bad > 0) ?
+      <Statistics good={good} neutral={neutral} bad={bad}/> 
+      : <Notification message="There is no feedback"></Notification> }          
+    </Section>
+    
+    
+    </>
+    
+    
+        )
+      }
+    
+  
 
 
-</>
-
-
-    )
-  }
-}
-
-
-export default App;
